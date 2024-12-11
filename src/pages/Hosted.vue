a85cbcef259801f4abab5785c1996fb6a2387583
victorgis <essangvictor@gmail.com>
2024-12-11 09:22:32 +0100

    added new stuff

---
+++ src/pages/Hosted.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="parent">
+    <Header />
+    <div class="main">
+      <section class="hosted">
+        
+      </section>
+    </div>
+    <Footer />
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "VeespatialUpdatedHosted",
+
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style scoped>
+.hosted {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
