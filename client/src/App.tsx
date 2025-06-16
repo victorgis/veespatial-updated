f2c982966710694c696ba5c72d3daee4f278bdbd
victorgis <essangvictor@gmail.com>
2025-06-16 20:55:55 +0100

    fe only

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,3 @@
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -7,14 +5,12 @@ import Home from "@/pages/home";
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
       <ThemeProvider>
         <TooltipProvider>
           <Toaster />
           <Home />
         </TooltipProvider>
       </ThemeProvider>
-    </QueryClientProvider>
   );
 }
 
