9bf8318e57714d90aeb0436f6d6cc63d06d62da1
victorgis <essangvictor@gmail.com>
2025-06-16 21:20:14 +0100

    added isa

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,5 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/queryClient";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { ThemeProvider } from "@/components/theme-provider";
@@ -5,12 +7,14 @@ import Home from "@/pages/home";
 
 function App() {
   return (
+    <QueryClientProvider client={queryClient}>
       <ThemeProvider>
         <TooltipProvider>
           <Toaster />
           <Home />
         </TooltipProvider>
       </ThemeProvider>
+    </QueryClientProvider>
   );
 }
 
